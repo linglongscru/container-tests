9aeb36926d4349d140a638a4afae582c02544b45
linglongmeng <linglong.meng@scrutinizer-ci.com>
2016-12-07 15:30:16 +0100

    update

--- saucelabtest.php
+++ saucelabtest.php
@@ -5,9 +5,8 @@
     use Facebook\WebDriver\Remote\RemoteWebDriver;
     use Facebook\WebDriver\WebDriverBy;
 
-    printf('saucelab identifier: '.getenv('SC_IDENTIFIER'));
     $web_driver = RemoteWebDriver::create(
-        "http://linglongscru:0fabcea5-5085-44c4-a246-f66ba570dc21@localhost:4444/wd/hub",
+        "https://linglongscru:0fabcea5-5085-44c4-a246-f66ba570dc21@ondemand.saucelabs.com:443/wd/hub",
         array("platform"=>"Windows 7", "browserName"=>"chrome", "version"=>"40", "tunnelIdentifier" => getenv('SC_IDENTIFIER'))
     );
 
